4fb70d44d6a40cdf9229e164f825d80389b18072
Nhanbn98 <nhann98@gmail.com>
2020-03-04 20:57:13 +0700

    edit book room

---
+++ app/src/main/java/com/example/booking/activity/ActivityBookRoom.java
@@ -0,0 +1,16 @@
+package com.example.booking.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.booking.R;
+
+public class ActivityBookRoom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_room);
+    }
+}
