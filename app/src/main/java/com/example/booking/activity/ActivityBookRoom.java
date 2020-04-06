ff7106e201810cab95612890f2a94d652b6319d4
Nhanbn98 <nhann98@gmail.com>
2020-04-06 14:18:48 +0700

    interface book room

--- app/src/main/java/com/example/booking/activity/ActivityBookRoom.java
+++ app/src/main/java/com/example/booking/activity/ActivityBookRoom.java
@@ -3,14 +3,51 @@ package com.example.booking.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.booking.R;
 
 public class ActivityBookRoom extends AppCompatActivity {
+    private Button btnPlus, btnLess, btnRegister;
+    private TextView lblNameTypeRoom, lblArc, lblPrice;
+    private EditText countRoom;
+    private int count = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_room);
+        init();
+        event();
     }
+
+    private void init() {
+        btnLess = findViewById(R.id.btnLess);
+        btnPlus = findViewById(R.id.btnPlus);
+        btnRegister = findViewById(R.id.btnRegister);
+        lblArc = findViewById(R.id.lblArc);
+        lblNameTypeRoom = findViewById(R.id.nameTypeRoom);
+        lblPrice = findViewById(R.id.price);
+        countRoom = findViewById(R.id.countRoom);
+    }
+
+    private void event() {
+//        eventChangeCountRoom();
+    }
+
+//    private void eventChangeCountRoom() {
+//            btnLess.setOnClickListener(v -> {
+//                count--;
+//                if (count <= 1){
+//                    count = 1;
+//                }
+//                countRoom.setText(String.valueOf(count));
+//            });
+//        btnPlus.setOnClickListener(v -> {
+//            count++;
+//            countRoom.setText(String.valueOf(count));
+//        });
+//    }
 }
